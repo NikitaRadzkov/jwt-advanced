c732d9f0bdd96149435824ca6de5f86dc76bee77
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-26 19:55:25 +0300

    WIP

--- service/user-service.js
+++ service/user-service.js
@@ -2,6 +2,8 @@ const UserModel = require('../models/user-model')
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mail-service')
+const tokensService = require('../service/token-service')
+const UserDto = require('../dtos/user-dto')
 
 class UserService {
     async registration(email, password) {
@@ -11,8 +13,18 @@ class UserService {
         }
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4()
+
         const user = await UserModel.create({email, hashPassword, activationLink})
         await mailService.sendActivationMail(email, activationLink)
+
+        const userDto = new UserDto(user)
+        const tokens = tokensService.generateTokens({...userDto})
+        await tokensService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 }
 
