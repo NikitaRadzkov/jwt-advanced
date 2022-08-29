73771f6c26404e49ef182d89cd81f3711b4ce8b5
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-29 19:31:17 +0300

    Updated mail and user service

--- service/user-service.js
+++ service/user-service.js
@@ -4,18 +4,19 @@ const uuid = require('uuid')
 const mailService = require('./mail-service')
 const tokensService = require('../service/token-service')
 const UserDto = require('../dtos/user-dto')
+const ApiError = require('../exceptions/api-error')
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email})
         if (candidate) {
-            throw new Error(`User ${email} already exist`)
+            throw new ApiError.BadRequest(`User ${email} already exist`)
         }
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4()
 
-        const user = await UserModel.create({email, hashPassword, activationLink})
-        await mailService.sendActivationMail(email, activationLink)
+        const user = await UserModel.create({email, password: hashPassword, activationLink})
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
 
         const userDto = new UserDto(user)
         const tokens = tokensService.generateTokens({...userDto})
@@ -25,6 +26,15 @@ class UserService {
             ...tokens,
             user: userDto
         }
+    }
+
+    async activate(activateLink) {
+        const user = await UserModel.findOne({activationLink})
+        if (!user) {
+            throw new ApiError.BadRequest('Wrong link')
+        }
+        user.isActivated = true
+        await user.save()
     }
 }
 
