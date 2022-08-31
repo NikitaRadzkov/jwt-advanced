d9a9c561226763695ee23a03b4ee86704875ec87
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-31 18:51:48 +0300

    Finish work with server

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -29,8 +29,9 @@ class UserController {
                 maxAge: 30 * 24 * 60 * 60 * 1000, 
                 httpOnly: true
             })
+            return res.json(token)
         } catch (e) {
-            
+            next(e)
         }
     }
 
@@ -57,7 +58,13 @@ class UserController {
 
     async refresh(req, res, next) {
         try {
-
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000, 
+                httpOnly: true
+            })
+            return res.json(userData)
         } catch (e) {
             next(e)
         }
@@ -65,7 +72,8 @@ class UserController {
 
     async getUser(req, res, next) {
         try {
-            res.json(['123', '456'])
+            const users = await userService.getAllUsers()
+            return res.json(users)
         } catch (e) {
             next(e)
         }
